8e109a5893c5d094f4afadf23760744310103af1
Vicky-ctrl <houwenhui8021@163.com>
2022-02-22 23:23:14 +0800

    提交index.py

---
+++ index/index.py
@@ -0,0 +1,31 @@
+# import main
+# from main import make_car
+# from main import make_car, build_profile
+# import main as m
+from main import *
+
+from restaurant import Restaurant
+# my_restaurant = Restaurant("Vicky's", "dessert")
+# my_restaurant.describe_restaurant()
+
+# from Upa import Admin
+# my_admin = Admin("Vicky", "Hou", location="China", age="8")
+# my_admin.privileges.show_privileges()
+
+# from pa import Admin
+#
+# my_admin = Admin("Vicky", "Hou", location="China", age="8")
+# my_admin.privileges.show_privileges()
+
+
+
+
+print("success!")
+
+
+
+
+
+
+
+
